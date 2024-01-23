3dee5b3581a853e01fbd7debb1bb172bba7c2bfe
binhchay1 <binhchay1@gmail.com>
2024-01-23 10:48:22 +0700

    ad preload video

--- wp-content/themes/gameleon/post-img-square.php
+++ wp-content/themes/gameleon/post-img-square.php
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
 		<?php $image = '<a href="' . get_permalink() . '">' . get_the_post_thumbnail(get_the_ID(), 'blog-square') . '</a>'; ?>
 		<?php if (empty($image[0])) $image[0] = myarcade_featured_image(); ?>
 		<?php echo html_entity_decode(esc_html($image)); ?>
-	<?php else : // show image placeholder
+	<?php else :
 	?>
 		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="img-thumb-game-with-video">
 			<img src="<?php echo get_post_meta(get_the_ID(), 'game_thumb1')[0] ?>" width="270" height="235" alt="<?php the_title(); ?>" />
@@ -28,8 +28,7 @@ if (!defined('ABSPATH')) {
 				<div class="foot-pre-load"></div>
 			<?php } ?>
 		</a>
-	<?php endif; // end of get_the_post_thumbnail 
+	<?php endif;
 	?>
-</div><?php // end of grid-image 
-		?>
+</div>
 <div class="clearfix"></div>
